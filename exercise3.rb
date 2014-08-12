b69a8361702e0845dfd9ea2088ffe0939f42f43d
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-12 15:06:02 -0400

    Third piece of working code

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
+#Greet the user
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+#Get his age and find out their year of birth
+puts "Mind me asking your age?"
+age = gets.chomp.to_i
+puts "Your year of birth should be #{Time.new.year - age}... Unless you've got multiple lives!"
